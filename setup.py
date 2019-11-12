f516964905a9c1ae7ad36ebd421fd536e4776ebb
Patrick McKeown <pmaccamp@sbcglobal.net>
2019-11-12 15:09:05 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-fixture-magic',
-    version='0.1.4',
+    version='0.2.0',
     description='A few extra management tools to handle fixtures.',
     long_description=open('README.rst').read(),
     author='Dave Dash',
